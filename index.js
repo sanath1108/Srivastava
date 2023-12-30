c9abba3e6a442d0929f00daa20a5c9977144349a
sanath1108 <127082945+sanath1108@users.noreply.github.com>
2023-12-30 23:07:00 +0530

    version1.0

---
+++ index.js
@@ -0,0 +1,11 @@
+function shinchan() {
+  window.open('why.html', '_self');
+}
+
+function hate() {
+  window.open('hate.html', '_self');
+}
+
+function conclusion() {
+  window.open('conclusion.html', '_self');
+}
